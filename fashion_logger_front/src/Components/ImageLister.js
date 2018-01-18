a4335d428d3487bad8e489f469425320d6ca1799
George Gore <georgelgore@gmail.com>
2018-01-18 15:40:55 -0500

    add semantic to topic

--- fashion_logger_front/src/Components/ImageLister.js
+++ fashion_logger_front/src/Components/ImageLister.js
@@ -4,10 +4,7 @@ import Image from "./Image";
 const ImageLister = props => {
   console.log("ImageLister,", props);
   return (
-    <div>
-      <h3>ImageLister</h3>
-      {props.images.map(img => <Image key={img.id} image={img} />)}
-    </div>
+    <div>{props.images.map(img => <Image key={img.id} image={img} />)}</div>
   );
 };
 
--- fashion_logger_front/src/Components/Topic.js
+++ fashion_logger_front/src/Components/Topic.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ImageLister from "./ImageLister";
+import { Card } from "semantic-ui-react";
 
 const Topic = props => {
   console.log("topic", props);
   return (
-    <div>
-      <h4>Topic: {props.topic.name}</h4>
+    <Card>
+      <h4 className="content">Topic: {props.topic.name}</h4>
       <ImageLister images={props.images} />
-    </div>
+    </Card>
   );
 };
 
