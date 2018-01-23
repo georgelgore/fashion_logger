753db8913f52fa07f07e91d6b4c0654de5bbd514
crutell331 <tcw331@gmail.com>
2018-01-23 17:09:57 -0500

    added styling to new User form

--- fashion_logger_front/src/Components/NewUser.js
+++ fashion_logger_front/src/Components/NewUser.js
@@ -1,37 +1,65 @@
 import React from "react";
+import { Container, Button } from "semantic-ui-react";
 
 const NewUser = props => {
   console.log("here");
   return (
-    <div style={{ marginTop: "5.55em" }}>
-      <form onSubmit={props.submitHandler}>
-        <label>Username:</label>
-        <input
-          type="text"
-          value={props.newUserInfo.username}
-          onChange={props.handleNewUser}
-          placeholder="username"
-          name="username"
-        />
-        <label>Email:</label>
-        <input
-          type="text"
-          value={props.newUserInfo.email}
-          onChange={props.handleNewUser}
-          placeholder="email"
-          name="email"
-        />
-        <label>Password:</label>
-        <input
-          type="password"
-          value={props.newUserInfo.password}
-          onChange={props.handleNewUser}
-          placeholder="password"
-          name="password"
-        />
-        <input type="submit" />
-      </form>
-    </div>
+    <Container>
+      <div
+        style={{
+          marginTop: "10.00em",
+          marginLeft: "30%",
+          marginRight: "30%",
+          maxWidth: "450px"
+        }}
+      >
+        <form className="ui large form" onSubmit={props.submitHandler}>
+          <Container size="small">
+            <div className="ui stacked segment">
+              <div className="field">
+                <div className="ui left icon input">
+                  <i className="user icon" />
+                  <input
+                    type="text"
+                    value={props.newUserInfo.username}
+                    onChange={props.handleNewUser}
+                    placeholder="username"
+                    name="username"
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <div className="ui left icon input">
+                  <i className="mail outline icon" />
+                  <input
+                    type="text"
+                    value={props.newUserInfo.email}
+                    onChange={props.handleNewUser}
+                    placeholder="email"
+                    name="email"
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <div className="ui left icon input">
+                  <i className="lock icon" />
+                  <input
+                    type="password"
+                    value={props.newUserInfo.password}
+                    onChange={props.handleNewUser}
+                    placeholder="password"
+                    name="password"
+                  />
+                </div>
+              </div>
+              <Button black="true" fluid>
+                Submit
+              </Button>
+            </div>
+          </Container>
+        </form>
+      </div>
+    </Container>
   );
 };
 
